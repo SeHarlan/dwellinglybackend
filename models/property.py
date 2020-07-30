a59c9df1678ca92f2219a058118188a88122fa98
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-07-29 20:41:09 -0700

    update seedData.py, PropertyModel return tenant IDs

--- models/property.py
+++ models/property.py
@@ -31,7 +31,7 @@ class PropertyModel(db.Model):
     def json(self):
         property_tenants = []
         for tenant in self.tenants:
-            property_tenants.append(tenant.json())
+            property_tenants.append(tenant.id)
 
         return {
             'id': self.id, 
@@ -41,11 +41,11 @@ class PropertyModel(db.Model):
             'state': self.state, 
             'zipcode': self.zipcode,
             'propertyManager': self.propertyManager,
-            'tenants': property_tenants,
+            'tenantIDs': property_tenants,
             'dateAdded': self.dateAdded,
             'archived': self.archived
         }
-    
+
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
